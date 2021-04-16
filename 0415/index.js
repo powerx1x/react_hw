6ce18492dcb18e00ff336dc6ddb666ae78a5a632
powerx1x <1106137109@nkust.edu.tw>
2021-04-17 02:31:56 +0800

    0415

---
+++ 0415/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Clock1 from './Clock1';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+    <Clock1 />,
+    document.getElementById('root')
+);
+
+
